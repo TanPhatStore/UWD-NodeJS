73b2343faf5f68fafaefa44c69341d40a9485ae8
Vũ Tiến Đức <107258145+TanPhatStore@users.noreply.github.com>
2023-06-24 16:56:54 +0700

    upload

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const cors = require('cors')
+const dotenv = require('dotenv')
+const db = require('./src/config/db')
+const authRouter = require('./src/routes/auth')
+const userRouter  = require('./src/routes/user')
+const app = express()
+const port = 8080 
+
+app.use(express.json())
+app.use(express.urlencoded({extended : true}))
+app.use('/uploads', express.static('uploads'));
+dotenv.config()
+app.use(cors())
+
+
+// Connect MongoDB
+db.connect()
+
+// Routes
+app.use('/v1/auth', authRouter)
+app.use('/v1/user', userRouter)
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
